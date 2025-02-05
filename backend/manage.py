4d335165242e5ee715bb2e14d2fcafcd106547ab
Muna Yusuf <muna369241@hotmail.com>
2025-02-05 09:15:30 +0000

    Fix incorrect app name to enable migrations

--- backend/manage.py
+++ backend/manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'return_work.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'return_to_work.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
