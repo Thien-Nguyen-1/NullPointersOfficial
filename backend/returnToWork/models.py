896ff6673b5f385947550153d60e0cec2f5f1453
Abiha-t <abiha.tasnim@gmail.com>
2025-02-08 16:38:22 +0000

    register ProgressTracker on admin.py and begin creation of progressTracker model

--- backend/returnToWork/models.py
+++ backend/returnToWork/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class ProgressTracker(models.Model):
+    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    Module = models.ForeignKey(Module, on_delete=models.CASCADE)
+    completed = models.BooleanField(default=False)
+
+
+
+def __str__(self):
+    return f"{self.user.username} - {self.module.title} - {'Completed' if self.completed else 'Incomplete'}"
