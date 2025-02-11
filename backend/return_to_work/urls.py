6817e8c165c76356e4c791c3f1870665ae0917a9
Abiha-t <abiha.tasnim@gmail.com>
2025-02-11 14:20:21 +0000

    urls.py

--- backend/return_to_work/urls.py
+++ backend/return_to_work/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
+from rest_framework.routers import DefaultRouter
+from returnToWork.views import ProgressTrackerViewSet
+
+router = DefaultRouter()
+router.register(r'progress_tracker', ProgressTrackerViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
