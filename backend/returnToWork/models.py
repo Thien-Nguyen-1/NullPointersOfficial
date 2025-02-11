f38e6a4eb8671f4012d8f2115afd1a9fb6b74d48
Fathima Maryam Risvi <161214725+maryam088@users.noreply.github.com>
2025-02-11 12:23:52 +0000

    Tag model created

--- backend/returnToWork/models.py
+++ backend/returnToWork/models.py
@@ -1,3 +1,21 @@
+from returnToWork.models.User import User
 from django.db import models
 
-# Create your models here.
+
+class Tags(models.Model):
+    """A class to create the model for tags, which are used to categorise courses and medical professional according to each issue"""
+    tag = models.CharField(max_length=50, unique=True, error_messages={ 'unique': "A tag with this name already exists." })
+
+    def save(self, *args, **kwargs):
+        """tag are normalised to lower case and stored"""
+        self.tag = self.tag.lower()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        """ Capitalize the first letter for display purposes"""
+        return self.tag.capitalize()
+
+    @classmethod
+    def get_valid_tags(cls):
+        """ Helper method to get list of valid tags"""
+        return list(cls.objects.values_list('tag', flat=True))
