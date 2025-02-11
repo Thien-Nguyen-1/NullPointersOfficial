2fd8414382d1754e22aa5d48d8ed28e42b2208c9
Tawyeeb Soetan <Tawyeebosoetan@Gmail.com>
2025-02-11 13:39:25 +0000

    Add Questionnaire Model
    Adds the questionnaire model

--- backend/returnToWork/models.py
+++ backend/returnToWork/models.py
@@ -1,3 +1,67 @@
 from django.db import models
+from django.core.exceptions import ValidationError
 
 # Create your models here.
+class Questionnaire(models.Model):
+    """Decision Tree like model to hold all the Yes/No questions in the questionnaire"""
+
+    question =  models.CharField(max_length=50, blank=False)
+    
+
+    # The following attributes are references to the next relevant questions based on the user's answers to the above question
+    yes_next_q = models.ForeignKey(
+        "self",
+        on_delete = models.SET_NULL,
+        null=True,
+        blank=True
+    )
+    no_next_q = models.ForeignKey(
+        "self",
+        on_delete = models.SET_NULL,
+        null=True,
+        blank=True
+    )
+
+
+    
+    def clean(self):
+        def is_parent_question(other):
+            """Helper function to test whether a given question is a parent to the current question (DFS)"""
+            
+            # Returns False is other is null
+            if not other:
+                return False
+
+
+            stack = [other]
+            
+            while stack:
+                head = stack[-1]
+
+                if head == other:
+                    return True
+                stack = stack[:-1]
+                
+                left_child = head.yes_next_q
+                right_child = head.no_next_q
+
+                if left_child:
+                    stack.append(left_child)
+                if right_child:
+                    stack.append(right_child)
+    
+            return False
+
+
+
+        def has_circular_references():
+            return is_parent_question(self.yes_next_q) or is_parent_question(self.no_next_q)
+            
+        if has_circular_references():
+            raise ValidationError("You cannot reference an ancestor question in a descendant question")
+
+                
+
+
+                
+
