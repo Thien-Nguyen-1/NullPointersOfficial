3ad9c1e0610505d2f76f46a98f6df3f5eae02fda
Abiha-t <abiha.tasnim@gmail.com>
2025-02-11 14:59:20 +0000

    begin test cases

--- backend/returnToWork/tests/models/test_progressTracker_model.py
+++ backend/returnToWork/tests/models/test_progressTracker_model.py
@@ -1,2 +1,40 @@
 from django.test import TestCase
-from returnToWork.models import ProgressTracker,Module,User #, Tag
+from returnToWork.models import ProgressTracker,Module,User
+
+class ProgressTrackerModelTest(TestCase):
+    
+    def setUp(self):
+        self.module = Module.objects.create(
+            title="Handling WORK anxiety",
+            description="This is a test module.",
+            pinned=True,
+            upvotes=9
+        )
+        self.user = User.objects.create_user(
+            username = '@jackdoe',
+            first_name = 'Jack',
+            last_name = 'Doe',
+            email = 'jackdoe@example.org',
+            password = 'SecurePass123',
+            user_type ='user',
+        )
+        self.progressTracker = ProgressTracker.objects.create(
+            User = '@jackdoe',
+            title = "Handling WORK anxiety",
+            completed = True,
+        )    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
