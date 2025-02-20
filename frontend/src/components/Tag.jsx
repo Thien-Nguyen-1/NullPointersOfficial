ca23919e829b53ff0ffb343fb2ffe36758d7f2e0
Abiha-t <abiha.tasnim@gmail.com>
2025-02-20 16:25:00 +0000

    fix design and hover of button

--- frontend/src/components/Tag.jsx
+++ frontend/src/components/Tag.jsx
@@ -1,26 +1,23 @@
 import React from 'react'
-import './Tags.css'; 
+import '../styles/Tags.css'; 
 const predefinedTags = [
     { id: 1, name: 'Unmotivated', description: 'unsure of identity at work' },
     { id: 2, name: 'Stuck', description: 'not knowing values' },
     { id: 3, name: 'Unclear Strengths', description: 'not knowing their strengths' },
-    { id: 4, name: 'Uncertain Future Plans', description: 'unsure of what their future plans are' },
+    { id: 4, name: 'Uncertain Future', description: 'unsure of what their future plans are' },
     { id: 5, name: 'Low Self-Esteem', description: 'low-self' }
   ];
 
- const Tags = () => {
-    return(
-        <div>
-            <h1>Tags that will be used to determine which module to take out of the 5 modules (ordered) </h1>
-      <ul>
-        {predefinedTags.map(tag => (
-          <li key={tag.id}>
-            <strong>{tag.name}</strong> - {tag.description}
-          </li>
-        ))}
-      </ul>
+  const Tags = () => {
+    return (
+        <div className="tags-container">
+            <h1>Predefined Tags</h1>
+            {predefinedTags.map(tag => (
+                <div className="tag" key={tag.id}>
+                    {tag.name}
+                </div>
+            ))}
         </div>
-    ); 
-
- }; 
+    );
+};
  export default Tags
