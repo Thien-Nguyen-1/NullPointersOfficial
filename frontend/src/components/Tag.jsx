fe066df094088c9f3b8743e2d603832cef1ded06
Abiha-t <abiha.tasnim@gmail.com>
2025-02-20 15:59:05 +0000

    create .jsx for tags

---
+++ frontend/src/components/Tag.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const predefinedTags = [
+    { id: 1, name: 'Unmotivated', description: 'unsure of identity at work' },
+    { id: 2, name: 'Stuck', description: 'not knowing values' },
+    { id: 3, name: 'Unclear Strengths', description: 'not knowing their strengths' },
+    { id: 4, name: 'Uncertain Future Plans', description: 'unsure of what their future plans are' },
+    { id: 5, name: 'Low Self-Esteem', description: 'low-self' }
+  ];
+
+ const Tags = () => {
+    return(
+        <div>
+            <h1>Tags that will be used to determine which module to take out of the 5 modules (ordered) </h1>
+      <ul>
+        {predefinedTags.map(tag => (
+          <li key={tag.id}>
+            <strong>{tag.name}</strong> - {tag.description}
+          </li>
+        ))}
+      </ul>
+        </div>
+    ); 
+
+ }; 
+ export default Tag
