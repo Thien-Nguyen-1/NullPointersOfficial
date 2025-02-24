6c66737a8c0b6fce05c08957430a71af3348ccee
Abiha-t <abiha.tasnim@gmail.com>
2025-02-24 15:32:51 +0000

    add usenavigate for buttons

--- frontend/src/components/CoursesList.jsx
+++ frontend/src/components/CoursesList.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "../styles/CoursesList.css"; // Import styles
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 
 const courses = [
