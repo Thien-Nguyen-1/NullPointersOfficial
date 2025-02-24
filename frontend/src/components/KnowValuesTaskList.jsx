079788efe1876049bd35abdd067d8dfea601715a
Abiha-t <abiha.tasnim@gmail.com>
2025-02-24 17:06:50 +0000

    experiment with design

--- frontend/src/components/KnowValuesTaskList.jsx
+++ frontend/src/components/KnowValuesTaskList.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import "../styles/CoursesList.css";
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
+
 
 const tasks =[
-    {title: "Know Your Values", progress: 2, button: "View Task"},
-    {title: "Be,Do, and Have", progress: 3, button: "View Task"},
+    {title: "Know Your Values", progress: 20, button: "View Task"},
+    {title: "Be,Do, and Have Exercise", progress: 89, button: "View Task"},
 ]
 
 const KnowValuesTaskList = () => {
@@ -13,6 +16,9 @@ const KnowValuesTaskList = () => {
         {tasks.map((task, index) => (
           <div key={index} className="course-card">
             <p>{task.title}</p>
+            <div style={{ width: 80, height: 80 }} className="progress-circle">
+                        <CircularProgressbar value={task.progress} text={`${task.progress}%`} />
+            </div>
             <button>{task.button}</button>
           </div>
         ))}
