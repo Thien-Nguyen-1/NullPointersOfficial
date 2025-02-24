50ad84f03a47c389ff9e53faa09352099b48e6d8
Muna Yusuf <muna369241@hotmail.com>
2025-02-24 23:32:05 +0000

    start styling

--- frontend/src/components/Welcome.jsx
+++ frontend/src/components/Welcome.jsx
@@ -1,16 +1,17 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import '../App.css';
+// import '../App.css';
+import '../styles/Welcome.css';
 
 const Welcome = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <h1 >Readiness to work</h1>
+    <div className="container">
+      <h1 >Empower</h1>
       <p >Please sign up or log in to continue.</p>
       
-      <div>
+      <div className="button-container">
         <button  onClick={() => navigate("/signup")}>
           Sign Up
         </button>
