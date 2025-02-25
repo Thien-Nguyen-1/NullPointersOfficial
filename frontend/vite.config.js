b2111d252cacdda0e20209777a10eea80fa2c0b6
Tawyeeb Soetan <Tawyeebosoetan@Gmail.com>
2025-02-25 11:35:38 +0000

    Add config for testing React components

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -9,4 +9,9 @@ export default defineConfig({
       "/api": "http://127.0.0.1:8000",
     },
   },
+  test : {
+    globals: true, 
+    setupFiles: "./tests/setupTests.js", 
+    environment: "jsdom",
+  },
 });
