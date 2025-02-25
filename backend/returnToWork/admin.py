d31be71013fefbdd5366a125518861f74ec452ff
Abiha-t <abiha.tasnim@gmail.com>
2025-02-25 13:27:20 +0000

    ensure Module and Task are added to admin.py

--- backend/returnToWork/admin.py
+++ backend/returnToWork/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
-from .models import ProgressTracker, User
+from .models import ProgressTracker, User,Module,Task
 
 # Register your models here.
 admin.site.register(ProgressTracker)
 admin.site.register(User)
+admin.site.register(Module)
+admin.site.register(Task)
