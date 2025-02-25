0dfdf612f905658653f0f94fcf46b990aafb8c19
Abiha-t <abiha.tasnim@gmail.com>
2025-02-25 14:13:10 +0000

    register Task,Content,InfoSheet,Video

--- backend/returnToWork/admin.py
+++ backend/returnToWork/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import ProgressTracker, User,Module,Task
+from .models import ProgressTracker, User,Module,Task,Content,InfoSheet,Video
 
 # Register your models here.
 admin.site.register(ProgressTracker)
 admin.site.register(User)
 admin.site.register(Module)
 admin.site.register(Task)
+admin.site.register(InfoSheet)
+admin.site.register(Video)
+admin.site.register(Content)
