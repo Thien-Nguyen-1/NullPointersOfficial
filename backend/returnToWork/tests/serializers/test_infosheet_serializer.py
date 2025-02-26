83c64c35c19ea981f86bb56df7114c85f4211bfa
Abiha-t <abiha.tasnim@gmail.com>
2025-02-26 23:22:12 +0000

    setup class data for test case

--- backend/returnToWork/tests/serializers/test_infosheet_serializer.py
+++ backend/returnToWork/tests/serializers/test_infosheet_serializer.py
@@ -2,4 +2,13 @@ from django.test import TestCase
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.contrib.auth import get_user_model
 from returnToWork.serializers import ContentSerializer,InfoSheetSerializer
-from returnToWork.models import User,Content,InfoSheet
+from returnToWork.models import User,Content,InfoSheet,Module
+
+User = get_user_model()
+
+
+class InfoSheetSerializerTest(TestCase):
+    @classmethod
+    def setUpClassData(cls):
+        cls.user = User.objects.create_user(username='john',password='testcase123')
+        cls.module = Module.objects.create(title='stress',description='dealing with work stress',pinned=False,upvote=1)
